8011608fa60ed9aa8683a1a8a205395a1b964751
HaykPetrosyan55 <haykpetrosyan55@users.noreply.github.com>
2018-06-26 19:38:11 +0400

    newProject

---
+++ HomeWorkW1_1/app/src/main/java/com/example/hp/homeworkw1_1/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.hp.homeworkw1_1;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button = findViewById(R.id.button);
+        final EditText editText = findViewById(R.id.edittext);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://" + editText.getText().toString()));
+                startActivity(intent);
+            }
+        });
+    }
+}
